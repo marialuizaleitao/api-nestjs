3961f498baf7c60136698ec80389524a9df96bec
Maria Leitão <malufln.99@gmail.com>
2024-08-06 15:57:54 -0300

    feat: add getters methods unit tests for UserEntity

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,6 +1,5 @@
 import { faker } from '@faker-js/faker';
 import { UserEntity, UserProps } from '../../user.entity';
-import e from 'express';
 
 describe('User entity unit tests', () => {
   let props: UserProps;
@@ -20,6 +19,29 @@ describe('User entity unit tests', () => {
     expect(sut.props.name).toEqual(props.name);
     expect(sut.props.email).toEqual(props.email);
     expect(sut.props.password).toEqual(props.password);
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+
+  it('Name getter', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+
+  it('Email getter', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toBe('string');
+  });
+
+  it('Password getter', () => {
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toEqual(props.password);
+    expect(typeof sut.props.password).toBe('string');
+  });
+
+  it('CreatedAt getter', () => {
+    expect(sut.props.createdAt).toBeDefined();
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
 });
